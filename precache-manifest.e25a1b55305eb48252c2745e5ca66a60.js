3a0481402d351e28503eac4a61ebc16b3451a2a6
Artur <brabus-20@mail.ru>
2020-07-13 21:18:56 +0300

    Updates

---
+++ precache-manifest.e25a1b55305eb48252c2745e5ca66a60.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79d9d4b3f9e597a8111d70ebd080cef",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "0992e5534ef9e1166c9c",
+    "url": "/react-project/static/css/4.4a5d517c.chunk.css"
+  },
+  {
+    "revision": "b699ca7e42e621691387",
+    "url": "/react-project/static/css/5.f179d09c.chunk.css"
+  },
+  {
+    "revision": "e0941a995a361df63fde",
+    "url": "/react-project/static/css/main.aa9b564d.chunk.css"
+  },
+  {
+    "revision": "7d01df57a4cef40026d5",
+    "url": "/react-project/static/js/0.19df4c8f.chunk.js"
+  },
+  {
+    "revision": "3ad03aaf1ac0b1f4228b",
+    "url": "/react-project/static/js/3.c006fabb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project/static/js/3.c006fabb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0992e5534ef9e1166c9c",
+    "url": "/react-project/static/js/4.79e610b9.chunk.js"
+  },
+  {
+    "revision": "b699ca7e42e621691387",
+    "url": "/react-project/static/js/5.7d420882.chunk.js"
+  },
+  {
+    "revision": "e0941a995a361df63fde",
+    "url": "/react-project/static/js/main.beba8f6c.chunk.js"
+  },
+  {
+    "revision": "cfcbbd686c66798a0b14",
+    "url": "/react-project/static/js/runtime-main.b2f4bee2.js"
+  },
+  {
+    "revision": "4ac030d69e58355914f9643ec1041d5a",
+    "url": "/react-project/static/media/avatarFemale.4ac030d6.png"
+  },
+  {
+    "revision": "deeb1527f89e4953286476d3b7bd4f1f",
+    "url": "/react-project/static/media/avatarMale.deeb1527.png"
+  },
+  {
+    "revision": "af31c3bee42454fee1ea0d372be80a69",
+    "url": "/react-project/static/media/mail.af31c3be.ico"
+  },
+  {
+    "revision": "7825312ef24d91dc795185d9dd212350",
+    "url": "/react-project/static/media/plus.7825312e.png"
+  },
+  {
+    "revision": "d8b3f08b8a6fafbd91b54fd1515c0afd",
+    "url": "/react-project/static/media/preloader.d8b3f08b.svg"
+  },
+  {
+    "revision": "c7508bf6c73b7e162b3d2ee1c2505d4e",
+    "url": "/react-project/static/media/print.c7508bf6.png"
+  },
+  {
+    "revision": "33eda13a502edf1e6c5fbf2121bbbe61",
+    "url": "/react-project/static/media/rocket.33eda13a.png"
+  }
+]);
